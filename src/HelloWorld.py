a268dabf90af88a646dddd14db8efabe6c360c47
youngqq <youngqqcn@163.com>
2017-08-21 20:24:58 +0800

    1.first commit; 2.add hello world programe for test.

---
+++ src/HelloWorld.py
@@ -0,0 +1,23 @@
+#!coding:utf8
+
+'''
+Date:2017/8/21/20:23
+Author:yqq
+Description:none
+'''
+
+
+
+def main():
+
+
+	print("hello world")
+
+	pass
+
+
+if __name__ == "__main__":
+
+	main()
+
+	pass
