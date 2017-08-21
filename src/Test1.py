fd17a2b4ffa1f698454159542720173075b978da
youngqq <youngqqcn@163.com>
2017-08-21 20:27:38 +0800

    1.first commit; 2.add hello world programe for test.

---
+++ src/Test1.py
@@ -0,0 +1,28 @@
+#!coding:utf8
+
+'''
+Date:2017/8/21/20:25
+Author:yqq
+Description:none
+'''
+
+def foo(n):
+	if 1  == n:
+		return 1
+	else:
+		return  foo(n-1) + foo(n-2)
+	pass
+
+
+def main():
+
+	print(foo(10))
+
+	pass
+
+
+if __name__ == "__main__":
+
+	main()
+
+	pass
